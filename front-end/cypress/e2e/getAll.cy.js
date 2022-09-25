3d9cbca3ea6598230c4e83c47e20e9f321a849b5
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-09-25 20:23:13 -0300

    test: (frontend) - GET /recommendations e2e test, should get all recommendations with limit 10

---
+++ front-end/cypress/e2e/getAll.cy.js
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker'
+
+beforeEach(() => {
+    cy.request('POST', 'http://localhost:5000/e2e/reset', {})
+})
+
+describe('Get All Recommendation', () => {
+    it('should get all(10) recommendations', () => {
+        let recommendations = []
+
+        for (let i = 0; i < 10; i++) {
+            const recommendation = {
+                name: faker.lorem.words(2),
+                youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}`
+            }
+
+            recommendations.push(recommendation)
+
+            cy.createRecommendation(recommendation)
+        }
+
+        cy.visit('http://localhost:3000')
+
+        cy.intercept('GET', `/recommendations`).as('getRecommendations')
+
+        cy.wait('@getRecommendations')
+
+        for (let i = 0; i < recommendations.length; i++) {
+            cy.get(`[data-cy-name="${recommendations[i].name}"]`).should('contain.text', recommendations[i].name)
+        }
+    })
+})
--- front-end/src/hooks/useAsync.js
+++ front-end/src/hooks/useAsync.js
@@ -21,8 +21,6 @@ export default function useAsync(handler, immediate = true) {
     if (immediate) {
       act();
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
